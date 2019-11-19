88fbed213dd451de7becfdb3c97b02b25e532e48
AntonKomarov <anton.komaro116@gmail.com>
2019-11-19 11:09:34 +0300

    FirstProgram

--- src/com/anton/calculator/Matrix.java
+++ src/com/anton/calculator/Matrix.java
@@ -3,4 +3,26 @@ package com.anton.calculator;
 public class Matrix extends Var{
 
     private double[][] matrix;
+
+    public Matrix(double[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public Matrix(String strMatrix) {
+        String[] strings = strMatrix.split(",");
+        this.matrix = new double[strings.length][strings.length];
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < this.matrix.length; j++) {
+                this.matrix[i][j] = Double.parseDouble(strings[i]);
+            }
+        }
+    }
+
+    public Matrix(Matrix matrix1) {
+    }
+
+    @Override
+    public String toString(){
+        return "This Matrix constructor";
+    }
 }
