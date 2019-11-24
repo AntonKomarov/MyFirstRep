4a495e8f600b5d2838bd8329c00b7c0170c626be
AntonKomarov <anton.komaro116@gmail.com>
2019-11-24 15:42:05 +0300

    Collection

--- src/com/anton/collection/TaskB1.java
+++ src/com/anton/collection/TaskB1.java
@@ -1,25 +1,50 @@
 package com.anton.collection;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
-public class TaskB1 {
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 extends Text {
+    private static String[] w = {};
+    private static int[] count = {};
+
     public static void main(String[] args) {
 
+        Map<Integer, String> map = new HashMap<>();
 
-        List<Integer> list = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+        Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]+");
+        Matcher matcher = pattern.matcher(text);
 
-        while (true) {
-            String word = scanner.next();
-            if (word.equals("end")) {
-                break;
+        while(matcher.find()){
+            String word = matcher.group();
+            int p = pos(word);
+            if (p >= 0){
+                count[p]++;
             }
-            Integer i = Integer.valueOf(word);
-            list.add(i);
+            else {
+                int last = w.length;
+                w = Arrays.copyOf(w, last+1);
+                w[last] = word;
+                count = Arrays.copyOf(count, last+1);
+                count[last] = 1;
+            }
         }
 
 
+
+        for (int i = 0; i < w.length; i++) {
+            System.out.println(w[i] + "=" + count[i]);
+        }
+    }
+
+    private static int pos(String word){
+        for (int i = 0; i < w.length; i++) {
+            if (w[i].equals(word)){
+                return i;
+            }
+        }
+        return -1;
     }
 }
+
