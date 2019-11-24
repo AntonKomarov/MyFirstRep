64b09fb958f0bdb59f379067819809f75a622cea
AntonKomarov <anton.komaro116@gmail.com>
2019-11-24 11:38:06 +0300

    Collection

---
+++ src/com/anton/collection/TaskA3.java
@@ -0,0 +1,28 @@
+package com.anton.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> listArr = new ArrayList<>();
+
+        int posZero = 0;
+
+        while (true) {
+            String word = scanner.next();
+            if (word.equals("end"))
+                break;
+            Integer i = Integer.valueOf(word);
+            if (i < 0)
+                listArr.add(i);
+            else  if (i == 0)
+                listArr.add(posZero, i);
+            else if (i > 0)
+                listArr.add(posZero++, i);
+        }
+        System.out.println(listArr);
+    }
+}
