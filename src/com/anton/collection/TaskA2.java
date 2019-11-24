187876798c844a81a86cf339f651bc4c5bd053d1
AntonKomarov <anton.komaro116@gmail.com>
2019-11-24 11:13:18 +0300

    Collection

--- src/com/anton/collection/TaskA2.java
+++ src/com/anton/collection/TaskA2.java
@@ -7,16 +7,26 @@ import java.util.TreeSet;
 
 public class TaskA2 {
 
-    public static void getCross(){
-
+    private static Set<Integer> getCross(Set<Integer> left, Set<Integer> right){
+        Set<Integer> cross = new HashSet<>(left);
+        cross.retainAll(right);
+        return cross;
+    }
+    private static Set<Integer> getUnion(Set<Integer> left, Set<Integer> right){
+        Set<Integer> union = new HashSet<>(left);
+        union.addAll(right);
+        return union;
     }
 
-    public static void getUnion(){
 
-    }
     public static void main(String[] args) {
         Set<Integer> left = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7));
         Set<Integer> right = new TreeSet<>(Arrays.asList(4,5,6,7,8,9,10,0));
+
+        System.out.println(left);
+        System.out.println(right);
+        System.out.println(getCross(left,right));
+        System.out.println(getUnion(left,right));
 
 
 
