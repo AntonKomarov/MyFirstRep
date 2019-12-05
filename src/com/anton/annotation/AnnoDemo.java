dbef0f46d251fd818e428b65f435073ee62bdd93
AntonKomarov <anton.komaro116@gmail.com>
2019-12-05 16:11:42 +0300

    Work with annotations

--- src/com/anton/annotation/AnnoDemo.java
+++ src/com/anton/annotation/AnnoDemo.java
@@ -2,8 +2,8 @@ package com.anton.annotation;
 
 @Anno(level = 3, sqlRequest = "Hello World!")
 public class AnnoDemo {
-    
-    void doAnno(){
+
+    private void doAnno(){
         Class<AnnoDemo> f = AnnoDemo.class;
         Anno a = f.getAnnotation(Anno.class);
         System.out.println("level = " + a.level());
