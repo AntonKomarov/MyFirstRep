7041fd6fe0b6a360767658adec4d21a33aa3e7e8
AntonKomarov <anton.komaro116@gmail.com>
2019-12-07 00:10:04 +0300

    Work with annotations

---
+++ src/com/anton/LazyService/Service.java
@@ -0,0 +1,14 @@
+package com.anton.LazyService;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Service {
+    String name();
+    
+    boolean lazyload() default false;
+}
