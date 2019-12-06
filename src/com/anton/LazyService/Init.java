707c0343323a29633267315e15dbe551f39d10f7
AntonKomarov <anton.komaro116@gmail.com>
2019-12-07 00:11:55 +0300

    Work with annotations

---
+++ src/com/anton/LazyService/Init.java
@@ -0,0 +1,14 @@
+package com.anton.LazyService;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Init {
+
+    boolean suppressExeption() default false;
+}
