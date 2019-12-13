bfe924ed39e0d2ebdd43049246103a0bd7735e48
AntonKomarov <anton.komaro116@gmail.com>
2019-12-13 16:39:03 +0300

    Work at home

--- src/com/anton/collection/Bar.java
+++ src/com/anton/collection/Bar.java
@@ -1,26 +1,44 @@
 package com.anton.collection;
 
-class Super{
-    Super(){
-        System.out.println("Super contructor");
-    }
-}
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
-public class Bar extends Super{
-    Bar(){
-        this(1);
-        System.out.println("Main() contructor");
-    }
+        class Test {
+            public static void main(String arg[]) {
+                Test.log("Hi");
+            }
 
-    Bar(int i){
-        System.out.println("Main(int) contructor");
-    }
 
-    public static void main(String[] args) {
-        new Bar();
-    }
-
-}
+            private static void log(String text){
+                PrintWriter out;
+                /*  Сначала откроем файл, в который будем писать ошибки */
+                try {
+                    out = new PrintWriter(new FileWriter("exceptions.log", true));
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    return;
+                }
+                try {
+        /*  Тут находится основной код вашего приложения. Ошибки будут
+            проявляться в виде исключений, например так (искуственный
+            пример)
+        */
+                    out.append(text).append("\n");
+                    throw new Exception();
+                } catch (Exception ex) {
+        /*  Перехватываем все необработанные исключения и пишем в логфайл
+            временную отметку, сообщение об ошибке и стектрейс (в котором
+            будут указаны методы, которые привели к ошибке и номера строк
+            в исходниках)
+        */
+                    out.printf("\n%s: %s\n", LocalDateTime.now(), ex.getMessage());
+                    ex.printStackTrace(out);
+                    out.flush();
+                }
+            }
+        }
 
 
 
