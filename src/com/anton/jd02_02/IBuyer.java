c5b9dbf68c7d1cdb5e0b103c69e514fce2de6900
AntonKomarov <anton.komaro116@gmail.com>
2019-12-15 19:29:21 +0300

    Work at home

---
+++ src/com/anton/jd02_02/IBuyer.java
@@ -0,0 +1,8 @@
+package com.anton.jd02_02;
+
+public interface IBuyer {
+    void enterToMarket();
+    void chooseGoods();
+    void goOut();
+    void getToQueue();
+}
---
+++ src/com/anton/jd02_02/IUseBacket.java
@@ -0,0 +1,6 @@
+package com.anton.jd02_02;
+
+public interface IUseBacket {
+    void takeBacket();
+    void putGoodsToBacket();
+}
