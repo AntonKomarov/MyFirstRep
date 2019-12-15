32b44709f972d69c2415d5371e6d4467ccd628f3
AntonKomarov <anton.komaro116@gmail.com>
2019-12-15 20:58:30 +0300

    Work at home

--- src/com/anton/jd02_01/Cashier.java
+++ src/com/anton/jd02_01/Cashier.java
@@ -11,6 +11,12 @@ public class Cashier implements Runnable {
     @Override
     public void run(){
         System.out.println(this + " cashier open");
+        Buyer b;
+        while((b = QueueBuyer.extractBuyer()) != null){
+            System.out.println(this + " service " + b);
+            Randomize.sleep(Randomize.fromTo(2000, 5000));
+            System.out.println(b + "  ");
 
+        }
     }
 }
