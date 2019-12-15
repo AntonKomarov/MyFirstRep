d6ed6c509e00529a6953634c74466fe39963f028
AntonKomarov <anton.komaro116@gmail.com>
2019-12-16 00:53:09 +0300

    Work at home

--- src/com/anton/jd02_05/MyLocale.java
+++ src/com/anton/jd02_05/MyLocale.java
@@ -14,18 +14,18 @@ public class MyLocale {
                 case '1':
                     country = "RU";
                     language = "RU";
-                    System.out.println("\n" + i + "- русский");
+                    System.out.println("\n" + i + " - русский");
                     break;
                 case '2':
                     country = "BY";
                     language = "BE";
-                    System.out.println("\n" + i + "- Белорусский");
+                    System.out.println("\n" + i + " - Белорусский");
                     break;
                 default:
-                    System.out.println("\n" + i + "- English");
+                    System.out.println("\n" + i + " - English");
             }
             Locale current = new Locale(language, country);
-            ResourceBundle rb = ResourceBundle.getBundle("com.anton.jd02_05.ok.str", current);
+            ResourceBundle rb = ResourceBundle.getBundle("com.anton.jd02_05", current);
             String s1 = rb.getString("str1");
             System.out.println(s1);
             String s2 = rb.getString("str2");
