f275deaead155429e9da99ba424e92e7893082e2
AntonKomarov <anton.komaro116@gmail.com>
2019-12-18 15:15:43 +0300

    Homework

---
+++ src/com/anton/prog_hub/Task_04.java
@@ -0,0 +1,13 @@
+package com.anton.prog_hub;
+
+public class Task_04 {
+    int total = 0;
+
+    public static void main(String[] args) {
+        (new Task_04()).doIt();
+    }
+
+    void doIt() {
+        System.out.println(total);
+    }
+}
