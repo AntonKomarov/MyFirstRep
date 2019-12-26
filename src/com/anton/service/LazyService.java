7e5f743db6033b436815a83613f37342d5459191
AntonKomarov <anton.komaro116@gmail.com>
2019-12-26 23:25:09 +0300

    Homework

--- src/com/anton/service/LazyService.java
+++ src/com/anton/service/LazyService.java
@@ -7,7 +7,7 @@ public class LazyService {
     public void start() throws Exception{
         System.out.println("Lazy Service started");
     }
-
+    
     public void stop(){
         System.out.println("Lazy Service stoped");
     }
