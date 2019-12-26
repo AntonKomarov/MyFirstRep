0859f356e5181ccca339fc3334bb35b454ef10c0
AntonKomarov <anton.komaro116@gmail.com>
2019-12-26 23:23:48 +0300

    Homework

--- src/com/anton/prog_hub/Task_13.java
+++ src/com/anton/prog_hub/Task_13.java
@@ -10,7 +10,7 @@ public class Task_13 {
         Map<String, Integer> map1 = new HashMap<>();
         Map<String, Integer> map2 = new HashMap<>();
 
-        map1.put("Number1", new Integer(100));
+        map1.put("Number1", 100);
         map1.put("Number2", new Integer(200));
         map1.put("Number3", new Integer(300));
 
