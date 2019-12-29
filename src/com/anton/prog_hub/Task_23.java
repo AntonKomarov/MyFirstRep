6beeda8c73e11cd25bb62c96ea5ce56d58f26235
AntonKomarov <anton.komaro116@gmail.com>
2019-12-29 22:42:54 +0300

    Homework

---
+++ src/com/anton/prog_hub/Task_23.java
@@ -0,0 +1,16 @@
+package com.anton.prog_hub;
+
+public class Task_23 {
+    public static boolean methodOne(){
+        System.out.println("methodOne");
+        return false;
+    }
+
+    public static boolean methodTwo(){
+        System.out.println("methdTwo");
+        return true;
+    }
+    public static void main(String[] args) {
+
+    }
+}
