1b8b0f11d7c50b47032dd9c4c072614aae34e81d
AntonKomarov <anton.komaro116@gmail.com>
2019-12-30 11:28:56 +0300

    Homework

--- src/com/anton/prog_hub/Task_23.java
+++ src/com/anton/prog_hub/Task_23.java
@@ -17,6 +17,5 @@ public class Task_23 {
     }
     public static void main(String[] args) {
         System.out.println(Task_23.methodOne() || Task_23.methodTwo() || Task_23.methodThree());
-
     }
 }
