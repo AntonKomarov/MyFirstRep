ba2c8ed686debffffd8ad7471b0b7805821fac11
AntonKomarov <anton.komaro116@gmail.com>
2020-01-08 23:20:27 +0300

    Homework

---
+++ src/com/anton/prog_hub/Task30.java
@@ -0,0 +1,30 @@
+package com.anton.prog_hub;
+
+public class Task30 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class At{
+    At(){}
+
+    At(int x){
+        a(x);
+    }
+
+    void a(int x){
+        System.out.println("A-a: " + x);
+    }
+}
+
+class Bt extends At{
+//    чтобы не было ошибки компиляци необходим конструктор по умолчанию
+    Bt(int x){
+        a(x);
+    }
+
+    void a(int x){
+        System.out.println("B-a: " + x);
+    }
+}
