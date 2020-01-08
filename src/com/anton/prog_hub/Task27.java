c8605f529072d8b6efe189c9d11d0a69084aa564
AntonKomarov <anton.komaro116@gmail.com>
2020-01-08 21:13:12 +0300

    Homework

--- src/com/anton/prog_hub/Task27.java
+++ src/com/anton/prog_hub/Task27.java
@@ -1,4 +1,17 @@
 package com.anton.prog_hub;
 
 public class Task27 {
+
+    static class X{
+        static String x = Y.y;
+    }
+
+    static class Y{
+        static String y = X.x;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Y.y = "0";
+        System.out.println(X.x);
+    }
 }
