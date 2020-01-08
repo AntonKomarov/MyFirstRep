fee98b1bc519b1f3b6e523488607de7c5be11fd9
AntonKomarov <anton.komaro116@gmail.com>
2020-01-09 00:05:45 +0300

    Homework

---
+++ src/com/anton/prog_hub/Singleton.java
@@ -0,0 +1,21 @@
+package com.anton.prog_hub;
+
+class Singleton {
+    private static Singleton s = new Singleton();
+    protected Singleton(){
+        System.out.println("S");
+    }
+    public static Singleton getInstance(){
+        return s;
+    }
+}
+
+class Testt{
+    public void method(){
+        Singleton.getInstance();
+    }
+    public static void main(String[] str){
+        new Testt();
+        new Singleton();
+    }
+}
