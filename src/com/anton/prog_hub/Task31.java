62e942bebe9b386ec327f6abbe7efde56e9bc09d
AntonKomarov <anton.komaro116@gmail.com>
2020-01-09 00:22:21 +0300

    Homework

--- src/com/anton/prog_hub/Task31.java
+++ src/com/anton/prog_hub/Task31.java
@@ -2,7 +2,7 @@ package com.anton.prog_hub;
 
 public class Task31 {
     public static void main(String[] args) {
-        Boolean b = new Boolean("Hello");
+        Boolean b = Boolean.valueOf("Hello");
         System.out.println(b);
         Boolean b1 = new Boolean("tRue");
         System.out.println(b1);
