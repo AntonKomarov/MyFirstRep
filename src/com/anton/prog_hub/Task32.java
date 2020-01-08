f1759cb6f1c1c554aab8b2fed36c94048153178c
AntonKomarov <anton.komaro116@gmail.com>
2020-01-09 00:28:43 +0300

    Homework

---
+++ src/com/anton/prog_hub/Task32.java
@@ -0,0 +1,13 @@
+package com.anton.prog_hub;
+
+public class Task32 {
+    private int anInt = 4;
+
+    public Task32(){
+        anInt = 5;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
