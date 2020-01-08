0640f4dd291ec8cf73f68cf86bf079d3e58b8c70
AntonKomarov <anton.komaro116@gmail.com>
2020-01-08 21:51:33 +0300

    Homework

---
+++ src/com/anton/prog_hub/Task28.java
@@ -0,0 +1,21 @@
+package com.anton.prog_hub;
+
+public class Task28 {
+    int i = 0;
+    public int increment(){
+        return ++i;
+    }
+}
+
+public class Be extends Task28 {
+    int i = 1;
+    public int increment() {
+        return ++i;
+    }
+
+
+    public static void main(String[] args) {
+        Task28 b = new Task28();
+        System.out.println(b.increment());
+    }
+}
