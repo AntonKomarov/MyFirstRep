ab40b16e83b5267587b418d3d1457de7428ef413
AntonKomarov <anton.komaro116@gmail.com>
2020-01-10 00:11:22 +0300

    Homework

--- src/com/example/Pet2.java
+++ src/com/example/Pet2.java
@@ -6,7 +6,13 @@ class Pet2 extends Object{
     int weight;
 
     Pet2(int x, int y) {
-        // ызов конструктора базового класса Object
+        // вызов конструктора базового класса Object
+        super();
+        // инициализация переменных
+        this.x = 5;
+        this.y = 5;
+        this.weight = 10;
+        // вызов кода конструктора
         this.x = x;
         this.y = y;
     }
