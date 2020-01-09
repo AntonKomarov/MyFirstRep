b91b770be0633dbad73fcb70b3175c6f554ec031
AntonKomarov <anton.komaro116@gmail.com>
2020-01-09 18:28:59 +0300

    Homework

---
+++ src/com/anton/prog_hub/Task33.java
@@ -0,0 +1,20 @@
+package com.anton.prog_hub;
+
+public class Task33 {
+     static void printt(){
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Task33 b = new N();
+        b.printt();
+    }
+}
+
+class N extends Task33 {
+     static void printt(){
+        System.out.println("Bye");
+    }
+
+
+}
