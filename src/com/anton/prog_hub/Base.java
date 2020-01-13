c72d001a3836c2b5d180a67b5e2b71389847075e
AntonKomarov <anton.komaro116@gmail.com>
2020-01-13 17:22:15 +0300

    Homework

---
+++ src/com/anton/prog_hub/Base.java
@@ -0,0 +1,21 @@
+package com.anton.prog_hub;
+
+class Base {
+    Base(int i){
+        System.out.println("Base");
+    }
+}
+
+class Derived extends Base{
+    Derived(int i) {
+        super(i);
+    }
+
+    public static void main(String[] args) {
+        Derived d = new Derived(6);
+    }
+
+    void Derived(){
+        System.out.println("Derived");
+    }
+}
