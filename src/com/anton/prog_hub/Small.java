b97f0e37eb01253f3003a52fe88c6e3dd21985ee
AntonKomarov <anton.komaro116@gmail.com>
2020-01-13 17:09:26 +0300

    Homework

--- src/com/anton/prog_hub/Small.java
+++ src/com/anton/prog_hub/Small.java
@@ -6,3 +6,10 @@ public class Small {
         return 5;
     }
 }
+
+class Mediun extends Small{
+    protected Number growUp(){
+        System.out.println("I'm a small");
+        return 5;
+    }
+}
