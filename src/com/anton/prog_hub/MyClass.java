cde365e4d519013b349a94e515d1c043a62727fa
AntonKomarov <anton.komaro116@gmail.com>
2020-01-13 17:00:35 +0300

    Homework

--- src/com/anton/prog_hub/MyClass.java
+++ src/com/anton/prog_hub/MyClass.java
@@ -3,6 +3,11 @@ package com.anton.prog_hub;
 class MyClass {
     public static void main(String[] args) {
         int i = 2/3;
-        
+        System.out.println(i);
+    }
+
+    static {
+        int i = 12;
+        System.out.println(i + 100);
     }
 }
