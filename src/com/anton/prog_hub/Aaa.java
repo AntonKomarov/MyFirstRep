08fa5ecdb84d8755ab92858b4124e55f51894a11
AntonKomarov <anton.komaro116@gmail.com>
2020-01-14 17:57:32 +0300

    Homework

--- src/com/anton/prog_hub/Aaa.java
+++ src/com/anton/prog_hub/Aaa.java
@@ -2,4 +2,15 @@ package com.anton.prog_hub;
 
 public class Aaa {
     private double x = 2;
+    public static void multX(Aaa a, double n){
+        a.setX(a.getX() * n);
+    }
+
+    private void setX(double xn) {
+
+    }
+
+    private double getX() {
+        return x;
+    }
 }
