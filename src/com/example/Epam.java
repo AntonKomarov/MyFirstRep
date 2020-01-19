c15c42d09c3610072c29cb0e029635f41bc79c02
AntonKomarov <anton.komaro116@gmail.com>
2020-01-19 13:05:51 +0300

    Homework

--- src/com/example/Epam.java
+++ src/com/example/Epam.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class Epam {
     double m1 = 5_000_000.75;
+    int m4 = 5_000_000;
+    int m5 = 0_10; // получается число 8
 //    long m2 = _5_000_000;   5_000_000
 //    int m = 0b_1010;    0b1010;
-    int m4 = 5_000_000;
 }
