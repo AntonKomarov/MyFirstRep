256e8beb0c446bdb82fc6b75c165abc9ac9e53f4
CApp <anton.komarov116@gmail.com>
2020-01-27 17:06:04 +0300

    TCP/IP

--- src/com/tcp_ip/ServSocket.java
+++ src/com/tcp_ip/ServSocket.java
@@ -1,19 +1,22 @@
 package com.tcp_ip;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ServSocket {
 
     public static void main(String[] args) throws IOException {
         try(ServerSocket servSocket = new ServerSocket(8189);
         Socket socket = servSocket.accept()){
-            InputStream inputStream = socket.getInputStream(); // приходит поток данных. Информация пришла
-            OutputStream outputStream = socket.getOutputStream(); // отправляем поток данных. Информация ушла
 
+            Scanner scanner = new Scanner(socket.getInputStream()); // приходит поток данных. Информация пришла
+            PrintWriter writer = new PrintWriter(socket.getOutputStream()); // отправляем поток данных. Информация ушла
+
+//            InputStream inputStream = socket.getInputStream();
+//            OutputStream outputStream = socket.getOutputStream();
         }
     }
 }
