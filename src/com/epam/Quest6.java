f9bb080ab8153b6923f31aba57bff667d6c30720
AntonKomarov <anton.komarov116@gmail.com>
2020-03-10 10:49:05 +0300

    epam

---
+++ src/com/epam/Quest6.java
@@ -0,0 +1,13 @@
+package com.epam;
+
+public class Quest6 {
+    public static void main(String[] args) {
+        System.out.print("A");
+        main("java7");
+    }
+
+    public static void main(String args) {
+        System.out.println("B");
+    }
+}
+// sout: AB
