769674e149b4abddc8c1aaf73aef2ef8f87571ad
AntonKomarov <anton.komarov116@gmail.com>
2020-03-10 10:55:59 +0300

    epam Quest22

---
+++ src/com/epam/Quest22.java
@@ -0,0 +1,12 @@
+package com.epam;
+
+public class Quest22 {
+    String java = "Java 11";
+
+    public static void main(String[] args) {
+        Quest22 q = new Quest22();
+        System.out.println(q.java);
+
+        //System.out.println(java); ошибка, поле не static
+    }
+}
