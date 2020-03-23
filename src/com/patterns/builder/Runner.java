ba3297ed1d939d22d3a9c1e9742efdd7b624b0ec
AntonKomarov <anton.komarov116@gmail.com>
2020-03-23 23:25:07 +0300

    practice

--- src/com/patterns/builder/Runner.java
+++ src/com/patterns/builder/Runner.java
@@ -2,7 +2,9 @@ package com.patterns.builder;
 
 class Runner {
     public static void main(String[] args) {
-        SportCar sportCar = new SportCar.Builder("Audi").setColor("Red").setMaxSpeed(300).build();
+        SportCar sportCar = new SportCar.Builder()
+                .setName("Audi")
+                .build();
         System.out.println(sportCar.getName());
         System.out.println(sportCar.getColor());
         System.out.println(sportCar.getMaxSpeed());
