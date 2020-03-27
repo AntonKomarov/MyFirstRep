a5c59f34509c794e61913c54c0ffbae9df839d9c
AntonKomarov <anton.komarov116@gmail.com>
2020-03-27 07:17:15 +0300

    practice

--- src/main/java/com/my_jpa/Human.java
+++
@@ -1,5 +0,0 @@
-package com.my_jpa;
-
-public class Human {
-
-}
---
+++ src/main/java/com/my_jpa/Person.java
@@ -0,0 +1,11 @@
+package com.my_jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+
+}
