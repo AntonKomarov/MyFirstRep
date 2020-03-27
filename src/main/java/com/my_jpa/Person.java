08f411760236e9484ff3264801800c822296a93c
AntonKomarov <anton.komarov116@gmail.com>
2020-03-27 07:17:55 +0300

    practice

--- src/main/java/com/my_jpa/Person.java
+++ src/main/java/com/my_jpa/Person.java
@@ -1,11 +1,19 @@
 package com.my_jpa;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "person")
 public class Person {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+
 
 
 }
