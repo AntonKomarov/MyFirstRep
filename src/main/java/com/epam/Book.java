37dc3d207b01b7ee6fd5d10eb5b79d796f0166da
AntonKomarov <anton.komarov116@gmail.com>
2020-04-08 07:37:33 +0300

    practice

--- src/main/java/com/epam/Book.java
+++ src/main/java/com/epam/Book.java
@@ -1,8 +1,21 @@
 package com.epam;
 
-public class Book {
+class Book {
+    Book(){
+
+    }
     private String book;
     public void setBook(String boo){
         book = boo;
     }
+}
+
+class DF {
+    public static void main(String[] args) {
+        String s1 = new String("Test");
+        String s2 = "Test";
+        System.out.println(s1 == s2);
+    }
+
+
 }
