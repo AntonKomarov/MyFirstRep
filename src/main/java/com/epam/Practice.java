2c40f09790d851131bfe5e4579705b716848d9d2
AntonKomarov <anton.komarov116@gmail.com>
2020-04-10 15:16:48 +0300

    practice

--- src/main/java/com/epam/Practice.java
+++ src/main/java/com/epam/Practice.java
@@ -1,39 +1,27 @@
 package com.epam;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class Practice {
 
-    public static Map<String,String> ret(Map<String,String> map) {
-        if(map.containsKey("ice cream")){
-            map.put("bread", "butter");
-            map.replace("ice cream", map.get("ice cream"), "cherry");
-        } else
-        if (map.size() == 0){
-            map.put("bread", "butter");
-        } else
-            if(!(map.containsKey("ice cream"))){
-                map.put("bread", "butter");
+    // количество повторяющися элементов
+    public static Map<String,Integer> ret(String[] strings) {
+        Map<String,Integer> map = new HashMap<>();
+        for (String word : strings) {
+            if(!map.containsKey(word)){
+                map.put(word,1);
+            } else {
+                int count = map.get(word);
+                map.put(word, count+1);
             }
+        }
         return map;
     }
 
-    public static void printMap(Map<String,String> map){
-        Iterator<Map.Entry<String,String>> iterator = map.entrySet().iterator();
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
-    }
-
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>(){{
-            put("ice cream", "candy");
-            put("b", "dirt");
-        }};
+        System.out.println(ret(new String[]{"hello", "hi", "Tanya", "hi"}));
 
-        printMap(ret(map));
     }
 }
 
