9a617a58c39d17291f0b38c5fe00baacc197d02e
AntonKomarov <anton.komarov116@gmail.com>
2020-04-24 10:34:25 +0300

    practice

--- src/main/java/com/practice/sort/Task1.java
+++ src/main/java/com/practice/sort/Task1.java
@@ -2,6 +2,9 @@ package com.practice.sort;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        float amount = 1.00000005F;
+        float avalue = 0.00000004F;
+        float result = amount - avalue;
+        System.out.println(result);
     }
 }
