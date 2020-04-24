a43e563c427da64911d38393cdfe376b11a2d25a
AntonKomarov <anton.komarov116@gmail.com>
2020-04-24 10:59:52 +0300

    practice

--- src/main/java/com/practice/sort/Task1.java
+++ src/main/java/com/practice/sort/Task1.java
@@ -2,9 +2,16 @@ package com.practice.sort;
 
 public class Task1 {
     public static void main(String[] args) {
-        float amount = 1.00000005F;
-        float avalue = 0.00000004F;
+        float amount = 1.0000005F;
+        float avalue = 0.0000004F;
         float result = amount - avalue;
         System.out.println(result);
+
+        double sqrt = Math.sqrt(-4);
+        System.out.println(sqrt + 1);
+        if (Double.isNaN(sqrt)) {
+            System.out.println("So sad");
+        }
+        System.out.println(Double.NaN == sqrt);
     }
 }
