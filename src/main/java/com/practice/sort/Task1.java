49cfb3d3ce359d6a12fa18438fd0fbc76c6b5529
AntonKomarov <anton.komarov116@gmail.com>
2020-04-25 23:58:38 +0300

    practice

--- src/main/java/com/practice/sort/Task1.java
+++ src/main/java/com/practice/sort/Task1.java
@@ -3,8 +3,7 @@ package com.practice.sort;
 public class Task1 {
     public static void main(String[] args) {
         if(null instanceof String){
-            System.out.println("instanceof true");
-        }
+            System.out.println("instanceof true"); }
         float amount = 1.0000005F;
         float avalue = 0.0000004F;
         float result = amount - avalue;
@@ -13,7 +12,6 @@ public class Task1 {
         System.out.println(sqrt + 1);
         if (Double.isNaN(sqrt)) { System.out.println("So sad"); }
         System.out.println(Double.NaN == sqrt);
-
         String costForPrint = "5$";
         System.out.println("Цена только для вас " +
                 + costForPrint.charAt(0) + getCurrencyName(costForPrint.charAt(1)));
