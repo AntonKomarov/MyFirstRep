19103f3a5c73aa66f2f3142b91237385cd571828
AntonKomarov <anton.komarov116@gmail.com>
2020-05-12 09:13:04 +0300

    practice

--- src/main/java/com/practice/sort/Task1.java
+++ src/main/java/com/practice/sort/Task1.java
@@ -15,7 +15,8 @@ public class Task1 {
         System.out.println("Цена только для вас " +
                 + costForPrint.charAt(0) + getCurrencyName(costForPrint.charAt(1))); }
     public static String getCurrencyName(char symbol) {
-        if (symbol == '$') { return " долларов"; }
+        if (symbol == '$') {
+            return " долларов"; }
         else {
             throw new UnsupportedOperationException("Not implemented yet"); } }
 }
