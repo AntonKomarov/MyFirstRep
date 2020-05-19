91ec595de24510c78425716896b61b91a3182d65
AntonKomarov <anton.komarov116@gmail.com>
2020-05-19 23:24:49 +0300

    practice

--- src/main/java/com/epam/collection/set/UseEnumSet.java
+++ src/main/java/com/epam/collection/set/UseEnumSet.java
@@ -10,6 +10,7 @@ public class UseEnumSet {
     public static void main(String[] args) {
         EnumSet<CarManufacturer> japanAuto = EnumSet.range(CarManufacturer.TOYOTA, CarManufacturer.SUZUKI);
         EnumSet<CarManufacturer> other = EnumSet.complementOf(japanAuto);
+
         System.out.println(japanAuto);
         System.out.println(other);
         action("audi", japanAuto);
